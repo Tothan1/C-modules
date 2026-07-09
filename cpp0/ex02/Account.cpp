f34834a7f488b318d1ea616979eba15be0afa984
tle-rhun <tle-rhun@f4r10s8.paris.42.school>
2026-07-09 12:26:35 +0200

    last to last ex02 for cpp0

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -1,8 +1,12 @@
-# include "Account.hpp"
+#include "Account.hpp"
 #include <iostream>
-#include <chrono>
 #include <ctime>
 
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 // GET
 int	Account::getNbAccounts( void )
@@ -15,72 +19,121 @@ int	Account::getTotalAmount( void )
 }
 int	Account::getNbDeposits( void )
 {
-	return _nbDeposits;
+	return _totalNbDeposits;
 }
 int	Account::getNbWithdrawals( void )
 {
-	return _nbWithdrawals;
+	return _totalNbWithdrawals;
 }
 
 // Other méthode
 void	Account::_displayTimestamp( void )
 {
-	auto now = std::chrono::system_clock::now();
-	std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-	std::cout << std::ctime(&currentTime);
+	time_t ctime = std::time(NULL);
+	struct tm ntime = *std::localtime(&ctime);
+
+	std::cout << '[' ;
+	std::cout << ntime.tm_year + 1900;
+	displayTimeFormat(ntime.tm_mon + 1, 2);
+	displayTimeFormat(ntime.tm_mday, 2);
+	std::cout << "_" ;
+	displayTimeFormat(ntime.tm_hour, 2);
+	displayTimeFormat(ntime.tm_min, 2);
+	displayTimeFormat(ntime.tm_sec, 2);
+	std::cout << ']';
+	// auto now = std::chrono::system_clock::now();
+	// std::cout << std::ctime(&currentTime);
+	// std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
 }
 
 void	Account::makeDeposit( int deposit )
 {
-
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";p_amount:" << _amount;
+	std::cout << ";deposit:" << deposit;
+	_amount += deposit;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";nb_deposits:" << _nbDeposits;
+	std::cout << std::endl;
 }
 bool	Account::makeWithdrawal( int withdrawal )
 {
-
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";p_amount:" << _amount;
+	std::cout << ";withdrawal:";
+	if(_amount - withdrawal > 0)
+	{
+		_amount -= withdrawal;
+		std::cout <<  withdrawal;
+		std::cout << ";amount:" << _amount;
+		_nbWithdrawals++;
+		std::cout << ";nb_withdrawals:" << _nbWithdrawals;
+		std::cout << std::endl;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+		return true;
+	}
+	else
+	{
+		std::cout << "refused";
+		std::cout << std::endl;
+		return false;
+	}
 }
 int	Account::checkAmount( void ) const
 {
-
+	return _amount;
 }
 
 // Principale méthode
 void	Account::displayStatus( void ) const
 {
-		std::cout << '[' << _displayTimestamp << ']';
-		std::cout << "index:" << _accountIndex;
-		std::cout << "amount:" << _amount;
-		std::cout << ';';
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";deposits:" << _nbDeposits;
+	std::cout << ";withdrawals:" << _nbWithdrawals;
+	std::cout << std::endl;
 }
 
-static void	Account::displayAccountsInfos( void )
+void	Account::displayAccountsInfos( void )
 {
-	for (int i = 0; i < _nbAccounts; i++)
-	{
-		std::cout << "deposits:" << _nbDeposits;
-		std::cout << "withdrawals:" << _nbWithdrawals;
-		std::cout << std::endl;
-	}
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts;
+	std::cout << ";total:" << _totalAmount;
+	std::cout << ";deposits:" << _totalNbDeposits;
+	std::cout << ";withdrawals:" << _totalNbWithdrawals;
+	std::cout << std::endl;
 }
 
 // Constructor
-
 Account::Account( int initial_deposit )
 {
-	for (int i = 0; i < _nbAccounts; i++)
-	{
-		Account::displayStatus();
-		std::cout << "created";
-		std::cout << std::endl;
-	}
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_amount = initial_deposit;
+	_totalAmount += _amount;
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";created";
+	std::cout << std::endl;
+	// _accountIndex++;
 }
 
 // Destructor
 Account::~Account( void )
 {
-	for (int i = 0; i < _nbAccounts; i++)
-	{
-		Account::displayStatus();
-		std::cout << "closed";
-		std::cout << std::endl;
-	}
+	Account::_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";closed";
+	std::cout << std::endl;
 }
