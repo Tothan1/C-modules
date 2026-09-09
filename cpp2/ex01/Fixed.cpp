bbb613ee4db7fb6c3af7ac0a1578278fd79607c3
tle-rhun <tle-rhun@f4r11s4.paris.42.school>
2026-09-09 12:50:47 +0200

    finish ex01

--- cpp2/ex01/Fixed.cpp
+++ cpp2/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: tle-rhun <tle-rhun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 12:47:57 by tle-rhun          #+#    #+#             */
-/*   Updated: 2026/09/08 17:11:37 by tle-rhun         ###   ########.fr       */
+/*   Updated: 2026/09/09 12:36:15 by tle-rhun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,33 @@
 Fixed::Fixed(void)
 {
 	std::cout << "Default constructor called" << std::endl;
-	number = 0;
+	_number = 0;
 }
-
+Fixed::Fixed(const int number)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_number = number * (1 << _number_const);
+}
+Fixed::Fixed(const float number)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_number = roundf(number * (1 << _number_const));
+}
 
 Fixed & Fixed::operator=(Fixed const & src)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->number = src.getRawBits();
+	this->_number = src._number;
 	// this->number_const = src.number_const;
 	return *this;
 }
+
+std::ostream& operator<<(std::ostream& os, Fixed const& fixed)
+{
+	os << fixed.toFloat();
+	return os;
+}
+
 
 Fixed::Fixed(Fixed const & src)
 {
@@ -38,6 +54,17 @@ Fixed::~Fixed()
 	std::cout << "Destructor called" << std::endl;
 }
 
+int Fixed::toInt( void ) const
+{
+	return(_number >> _number_const);
+}
+
+float Fixed::toFloat( void ) const
+{
+	return((float) _number / (1 << _number_const));
+}
+
+
 
 
 
@@ -45,11 +72,11 @@ Fixed::~Fixed()
 int Fixed::getRawBits() const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return (number);
+	return (_number);
 }
 
 void Fixed::setRawBits( int const raw )
 {
 	// std::cout << "Copy assignment operator called" << std::endl;
-	number = raw;
+	_number = raw;
 }
