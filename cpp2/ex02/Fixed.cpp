d9e06cab2278ac1d9d489340b61b0d5e01bae3d2
tle-rhun <tle-rhun@f6r5s9.paris.42.school>
2026-09-09 19:06:38 +0200

    à corriger

---
+++ cpp2/ex02/Fixed.cpp
@@ -0,0 +1,199 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-rhun <tle-rhun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 12:47:57 by tle-rhun          #+#    #+#             */
+/*   Updated: 2026/09/09 19:06:24 by tle-rhun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	// std::cout << "Default constructor called" << std::endl;
+	_number = 0;
+}
+Fixed::Fixed(const int number)
+{
+	// std::cout << "Int constructor called" << std::endl;
+	_number = number * (1 << _number_const);
+}
+Fixed::Fixed(const float number)
+{
+	// std::cout << "Float constructor called" << std::endl;
+	_number = roundf(number * (1 << _number_const));
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	// std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed()
+{
+	// std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::toInt( void ) const
+{
+	return(_number >> _number_const);
+}
+
+float Fixed::toFloat( void ) const
+{
+	return((float) _number / (1 << _number_const));
+}
+
+
+
+
+
+
+int Fixed::getRawBits() const
+{
+	// std::cout << "getRawBits member function called" << std::endl;
+	return (_number);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	_number = raw;
+}
+
+//OPERATOR
+Fixed & Fixed::operator=(Fixed const & src)
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	this->_number = src._number;
+	// this->number_const = src.number_const;
+	return *this;
+}
+
+bool  Fixed::operator>(Fixed const & a)
+{
+	if(this->_number > a.getRawBits())
+		return true;
+	else
+		return false;
+}
+bool  Fixed::operator<(Fixed const & a)
+{
+	if(this->_number < a.getRawBits())
+		return true;
+	else
+		return false;
+}
+bool  Fixed::operator>=(Fixed const & a)
+{
+	if(this->_number >= a.getRawBits())
+		return true;
+	else
+		return false;
+}
+bool  Fixed::operator<=(Fixed const & a)
+{
+	if(this->_number <= a.getRawBits())
+		return true;
+	else
+		return false;
+}
+bool  Fixed::operator==(Fixed const & a)
+{
+	if(this->_number == a.getRawBits())
+		return true;
+	else
+		return false;
+}
+bool  Fixed::operator!=(Fixed const & a)
+{
+	if(this->_number != a.getRawBits())
+		return true;
+	else
+		return false;
+}
+
+Fixed Fixed::operator+(Fixed const & a)
+{
+	return Fixed(this->_number + a.getRawBits());
+}
+Fixed Fixed::operator-(Fixed const & a)
+{
+	return Fixed(this->_number - a.getRawBits());
+}
+Fixed Fixed::operator*(Fixed const & a)
+{
+	return Fixed(this->toFloat() *a.toFloat());
+}
+Fixed Fixed::operator/(Fixed const & a)
+{
+	return Fixed(this->toFloat() / a.toFloat());
+}
+
+
+Fixed & Fixed::operator++(void)
+{
+	this->_number++;
+	return *this;
+}
+Fixed Fixed::operator++(int)
+{
+	Fixed a = *this;
+	this->_number++;
+	return a;
+}
+Fixed & Fixed::operator--(void)
+{
+	this->_number--;
+	return *this;
+}
+Fixed Fixed::operator--(int)
+{
+	Fixed a = *this;
+	this->_number--;
+	return a;
+}
+
+
+std::ostream& operator<<(std::ostream& os, Fixed const& fixed)
+{
+	os << fixed.toFloat();
+	return os;
+}
+
+
+//MIN
+Fixed const & Fixed::min(Fixed const &a, Fixed const & b)
+{
+	if(a.getRawBits() > b.getRawBits() )
+		return b;
+	else
+		return a;
+}
+Fixed & Fixed::min(Fixed  &a, Fixed  & b)
+{
+	if(a.getRawBits() > b.getRawBits() )
+		return b;
+	else
+		return a;
+}
+//MAX
+Fixed const & Fixed::max(Fixed const &a, Fixed const & b)
+{
+	if(a.getRawBits() > b.getRawBits() )
+		return a;
+	else
+		return b;
+}
+Fixed & Fixed::max(Fixed  &a, Fixed  & b)
+{
+	if(a.getRawBits() > b.getRawBits() )
+		return a;
+	else
+		return b;
+}
---
+++ cpp2/ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-rhun <tle-rhun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/11 16:09:26 by tle-rhun          #+#    #+#             */
+/*   Updated: 2026/09/09 16:09:20 by tle-rhun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
